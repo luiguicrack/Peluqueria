d1cffed91a5e31410d9bc1eee48a43afe973f6c6
JORGE <JORGE@DESKTOP-FJHAL2C>
2023-09-14 20:19:48 -0500

    Interfaz peluqueria

---
+++ src/main/java/com/mycompany/pelu/Lucrecia.java
@@ -0,0 +1,27 @@
+package com.mycompany.pelu;
+
+public class Lucrecia{
+    public int resultado =0;
+    public int cantidad =0;
+    
+    public void corteCaballero(){
+        resultado += 8000;
+        cantidad ++;
+    }
+    
+    public void corteDama(){
+        resultado += 10500;
+        cantidad ++;
+    }
+    
+    public void tintura(){
+        resultado += 40000;
+        cantidad ++;
+    }
+    
+    public void peinado(){
+        resultado += 15000;
+        cantidad ++;
+    }
+    
+}
